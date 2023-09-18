8547fa57eba2e2caf7382630bd9d072a72f9b77f
syd <sayed@kometsoft.com.my>
2023-09-18 14:57:25 +0800

    init

---
+++ users.php
@@ -0,0 +1,40 @@
+<?php
+
+$users = [
+    [
+        'id' => 1,
+        'name' => 'Mrs. Vilma Hara',
+        'email' => 'hhills@gmail.net',
+        'created_at' => '2023-07-20 07:29:42',
+        'updated_at' => '2023-07-20 07:2 9:42'
+    ],
+    [
+        'id' => 2,
+        'name' => 'Dr. Genesis Grady',
+        'email' => 'manley80@gmail.net',
+        'created_at' => '2023-07-20 07:29:42',
+        'updated_at' => '2023-07-20 07:2 9:42'
+    ],
+    [
+        'id' => 3,
+        'name' => 'Hal Halvorson',
+        'email' => 'sallie76@gmail.org',
+        'created_at' => '2023-07-20 07:29:42',
+        'updated_at' => '2023-07-20 07:2 9:42'
+    ],
+    [
+        'id' => 4,
+        'name' => 'Mrs. Sydnee Jenkins',
+        'email' => 'lexie.buckridge@gmail.net',
+        'created_at' => '2023-07-20 07:29:42',
+        'updated_at' => '2023-07-20 07:2 9:42'
+    ],
+    [
+        'id' => 5,
+        'name' => 'Mabel West',
+        'email' => 'hattie.carter@gmail.net',
+        'created_at' => '2023-07-20 07:29:43',
+        'updated_at' => '2023-07-20 07:2 9:43'
+    ],
+];
+
